43c6615fdd83341e1820fc28c2311005a9bef152
Nikolay Sokhan <sokhan.nikolay@gmail.com>
2021-06-21 21:08:06 +0300

    ms_tutorial_2 : update programm

--- ms_tutorial_2/Program.cs
+++ ms_tutorial_2/Program.cs
@@ -6,7 +6,22 @@ namespace ms_tutorial_2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-        }
+			String[] names = { "Eugene", "Katherina", "John", "Stephen", "Phil", "Joe", "Alex" };
+			int[] times = { 172, 169, 191, 202, 181, 191, 201 };
+			int max = times[0];
+			int index = 0;
+			for (int i = 0; i < times.Length; ++i) 
+			{
+				if (max < times[i])
+				{
+					max = times[i];
+					index = i;
+
+				}
+			}
+
+			Console.WriteLine("Name = {0} Height: {1}", names[index], max);
+			//while (true) { }
+		}   
     }
 }
