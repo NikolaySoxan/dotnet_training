636d2f6211db415ded7fbaa9254c2c0eafe0fd04
Nikolay Sokhan <sokhan,nikolay@gmail.com>
2021-06-21 19:34:51 +0300

    Init project

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace C__1._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+             Console.WriteLine("The current time is " + DateTime.Now);
+        }
+    }
+}
