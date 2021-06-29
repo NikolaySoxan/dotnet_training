b5a43376ce8860cabcfc538e73d556b746a6294a
Nikolay Sokhan <sokhan.nikolay@gmail.com>
2021-06-29 22:18:32 +0300

    Init project 3

---
+++ ms_tutorial_3/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ms_tutorial_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
