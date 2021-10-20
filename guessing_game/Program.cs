f7ecb66917673e341b5154a9d2bb4f24d8a5eb43
Nikolay Sokhan <sokhan.nikolay@gmail.com>
2021-10-20 23:54:16 +0300

    add guessing game

---
+++ guessing_game/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace guessing_game
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("enter random value:");
+            int name = Int32.Parse(Console.ReadLine());
+            Random x = new Random();
+            int value = x.Next(1, 12);
+            if(value==name)
+            {
+                Console.WriteLine("LUCKY");
+            }
+            else 
+            {
+                Console.WriteLine($"Loser random: {value}, Your value: {name}");
+            }
+              
+        }
+    }
+}
